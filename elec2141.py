1bc276009ebb1d754fe86d96786a4a93014cc36b
Zartarius <zaryabayaz2@gmail.com>
2025-05-06 19:10:39 +1000

    Update elec2141.py

--- elec2141.py
+++ elec2141.py
@@ -51,6 +51,25 @@ class boolean_expression:
             expression = expression.replace(param[0], param[2])
         return int(eval(expression))
         
+    def truth_table(self):
+        truth_table = ""
+        for param in self._params:
+            truth_table += f" {param} |"
+        truth_table += " OUT\n"
+        truth_table += ("-" * (len(self._params) * 5 + 2)) + "\n"
+        
+        inputs = 0
+        while inputs < (2 ** len(self._params)):
+            expression = self._expr
+            for i in range(len(self._params)):
+                bit = (inputs >> (len(self._params) - i)) & 1
+                truth_table += f" {bit} |"
+                expression = expression.replace(self._params[i], str(bit))
+            truth_table += f"  {int(eval(expression))}\n"
+            inputs += 1
+            
+        print(truth_table)
+        
 s = "A(B\\C\\ + (B+C)\\)\\"
 f = boolean_expression(s)
-print(f.evaluate("A=1, B=0, C=1"))
+f.truth_table()
