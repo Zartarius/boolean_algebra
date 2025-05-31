0ee7466d0453c9dcc9e8e770382a93cb775b521e
Zartarius <zaryabayaz2@gmail.com>
2025-05-31 15:36:19 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
-import boolmods
+from boolmods import *
 
-boolmods.set_complement("\\")
-expression = "AB + (C+D)\\"
-f = boolmods.boolean_expression(expression)
-min_max_terms = f.min_max_terms()
-print(f"Minterms: {min_max_terms["minterms"]}\nMaxterms: {min_max_terms["maxterms"]}")
-f.truth_table()
-SOP, POS = f.SOP_form(), f.POS_form()
-print(f"Sum of Products form: {SOP}\nProduct of Sums form: {POS}")
-GIC = f.GIC()
-print(f"Gate Input Cost: {GIC}")
-f.print_summary() # This does everything in one method
+set_complement("\\") # Optional, default complement is set to '
+f = boolean_expression("(AB)\\(B\\+C)+B(A\\C+AD\\)")
+f.print_summary()
 
-minterms = (1,2,3,4,5,6,7)
-dcs = (9, 10)
-params = ('A', 'B', 'C', 'D')
+minterms = (5,7,11,12,27,29)
+dcs = (14,20,21,22,23)
+params = ('A', 'B', 'C', 'D', 'E')
 g = boolean_terms(params, minterms=minterms, dcs=dcs)
-g.truth_table()
+g.print_summary()
